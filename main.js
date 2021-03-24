2370abb9d821f538194e7ed54223561c60c554be
Eliska_S <el.sibrova@gmail.com>
2021-03-24 16:43:23 +0100

    [Lekce 3]

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 // tady je místo pro náš program
+
+let kostka;
+
+function hodKostkou (selector, imageSelector){
+    kostka = Math.floor(Math.random()*6) + 1;
+    document.getElementById(imageSelector).src = "obrazky/" + kostka + ".png";
+    
+    if (kostka === 6) {
+        document.getElementById(selector).innerHTML = "Hodila jsi 6! Vyhrála jsi!!!";
+        document.querySelector('.tlacitko').innerHTML = "Vyhrála jsi!";
+    } else {
+        document.querySelector('.tlacitko').innerHTML= "Házej";
+        document.getElementById(selector).innerHTML = "Hodila jsi " + kostka + ". Házej znovu.";
+    }
+}
